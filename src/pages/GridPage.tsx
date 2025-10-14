73998cf9819e2e10ecd5956e980145e57e865a25
arsavag <arsavag94@gmail.com>
2025-10-14 15:17:49 +0400

    fixed some lint and tsconfigs for file paths.
    added main pages for grid and single photo page.

---
+++ src/pages/GridPage.tsx
@@ -0,0 +1,7 @@
+export default function GridPage() {
+  return (
+    <>
+      <h1>Grid Page</h1>
+    </>
+  )
+}
---
+++ src/routes.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+import GridPage from "@/pages/GridPage";
+import PhotoPage from "@/pages/PhotoPage";
+
+export const router = createBrowserRouter([{
+    path: "/",
+    element: <GridPage />
+},
+{
+    path: "/photo/:id",
+    element: <PhotoPage />
+}]);
